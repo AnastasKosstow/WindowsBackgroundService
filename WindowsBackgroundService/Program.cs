816ccb3f89a47ebe1504d6c9d07b2261416f06bb
Kosstow <anastas.kosstow@protonmail.com>
2021-10-20 07:46:38 +0300

    Initial

---
+++ WindowsBackgroundService/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace WindowsBackgroundService
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args)
+                .Build()
+                .RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
