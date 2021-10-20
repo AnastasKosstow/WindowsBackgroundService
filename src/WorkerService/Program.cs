ccee63783ac541fe137b8c3b21c358ac00c3e45e
Kosstow <anastas.kosstow@protonmail.com>
2021-10-20 08:50:49 +0300

    add RabbitMQ Client

--- src/WorkerService/Program.cs
+++ src/WorkerService/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
-namespace WindowsBackgroundService
+namespace WorkerService
 {
     public class Program
     {
--- src/WorkerService/Worker.cs
+++ src/WorkerService/Worker.cs
@@ -4,7 +4,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace WindowsBackgroundService
+namespace WorkerService
 {
     public class Worker : BackgroundService
     {
